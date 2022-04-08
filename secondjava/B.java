f0011a3652b96003ca811c18a956174528b4441b
prabodha <prabodhasenadhipathi123@gmail.com>
2022-04-08 13:31:40 +0530

    change3

--- secondjava/B.java
+++ secondjava/B.java
@@ -3,5 +3,6 @@ package java_learning_period.secondjava;
 public class B {
    public static void main(String[] args) {
        System.out.println("nfdjnfjd");
+       System.out.println("nbbbfdjnfjd"); 
    } 
 }
