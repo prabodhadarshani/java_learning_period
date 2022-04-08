415bfad474bff6efb160c5309e230ebc9a313604
prabodha <prabodhasenadhipathi123@gmail.com>
2022-04-08 13:23:04 +0530

    my first java github

--- java_learning_period/src/java_learning_period/Java_learning_period.java
+++ java_learning_period/src/java_learning_period/Java_learning_period.java
@@ -15,7 +15,7 @@ public class Java_learning_period {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("jfdjf");
+        System.out.println("jfdjddddf");
     }
     
 }
